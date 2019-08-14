c21e3410516709d8768f26e22d200fa69c343940
Alkaline Thunder <theplexgate@gmail.com>
2019-08-14 19:46:39 -0400

    Fix user page

--- src/UserPage.vue
+++ src/UserPage.vue
@@ -4,7 +4,10 @@
             <b-card no-body>
                 <b-card-body v-if="profile.fullName.length">
                     <b-card-title>{{ profile.fullName }}</b-card-title>
-                    <b-card-sub-title>{{ profile.username }}</b
+                    <b-card-sub-title>{{ profile.username }}</b-card-sub-title>
+                </b-card-body>
+                <b-card-body v-else>
+                    <b-card-title>{{ profile.username }}</b-card-title>
                 </b-card-body>
             </b-card>
         </b-container>
