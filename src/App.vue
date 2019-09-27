1b30ad53a052ec71b722f1d6abc8c29c126df751
Michael VanOverbeek <theplexgate@gmail.com>
2019-09-27 14:20:41 +0000

    I'm going to basically redo the theme.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" :class="theme()">
     <b-navbar toggleable="lg" type="dark" variant="dark" sticky>
-      <b-navbar-brand to="/">Project: Cassian</b-navbar-brand>
+      <b-navbar-brand to="/">Bit Phoenix Projects</b-navbar-brand>
         
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
